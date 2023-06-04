9904e1e5c21e95e2cd19c5517ccf974466096e2f
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-06-04 10:34:57 +0600

    task for lab final

---
+++ Sorting Algorithm/labfinal/quickshort psedudo.cpp
@@ -0,0 +1,15 @@
+
+function quicksort(arr, low, high):
+    if low < high:
+        pivot_index = partition(arr, low, high)
+        quicksort(arr, low, pivot_index - 1)
+        quicksort(arr, pivot_index + 1, high)
+
+function partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j = low to high - 1:
+        if arr[j] <= pivot:
+            swap(arr[++i], arr[j])
+    swap(arr[++i], arr[high])
+    return i + 1
